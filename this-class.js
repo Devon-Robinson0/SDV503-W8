b13f8d5c3d6dd31ca72a33c434df55d2df86f9b1
Devon-Robinson0 <devon-robinson2@live.nmit.ac.nz>
2026-04-29 11:46:16 +1200

    added a object for class practice about courses

---
+++ this-class.js
@@ -0,0 +1,10 @@
+const classNMIT = {
+    name: "SDV503",
+    tutor: "Ali Kahwaji",
+    tags: ["JavaScript", "TypeScript"],
+    level: 5,
+    qualification: "certificate",
+    year: 2026
+};
+
+console.log(classNMIT.name);
